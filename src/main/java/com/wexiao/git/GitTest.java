058fc1fe3b8e26235baf3b63a3d1620ae914b6ec
wexiao <461242289@qq.com>
2022-03-28 22:46:56 +0800

    second commit

--- src/main/java/com/wexiao/git/GitTest.java
+++ src/main/java/com/wexiao/git/GitTest.java
@@ -9,5 +9,6 @@ package com.wexiao.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
