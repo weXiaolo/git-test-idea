715576b311348203a5bf5b7a0aa8bf91f7ed46f8
wexiao <461242289@qq.com>
2022-03-28 22:21:45 +0800

    first commit

---
+++ src/main/java/com/wexiao/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.wexiao.git;
+
+/**
+ * Description: git-test-idea
+ * Created by wexiao on 2022/3/28 22:15
+ *
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
